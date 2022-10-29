7f82cc76494d263b7d056a393d8d78e810853e0e
Shahar <barr.shahar@gmail.com>
2022-10-29 16:12:36 +0300

    merge sort function is completed

--- stats.c
+++ stats.c
@@ -42,3 +42,104 @@ int main() {
 
 /* Add other Implementation File Code Here */
 
+void print_array(unsigned char* array, unsigned int length){
+    printf("[");
+    for (unsigned int i = 0; i < length - 1; ++i)
+        printf("%d ", array[i]);
+    printf("%d]\n", array[length-1]);
+}
+
+unsigned char find_median(unsigned char* array, unsigned int length){
+    unsigned char c_arr[length]; // copy array
+    for (unsigned int i = 0; i < length; ++i)
+        c_arr[i] = array[i];
+    
+    // sort array
+    sort_array(c_arr, length);
+    
+    // if length is odd, median = arr[N/2]
+    // if length is even, median = 1/2(arr[n\2] + arr[n/2-1])
+    if (length % 2 == 0) // length is even
+        return (c_arr[length/2] + c_arr[length/2-1])/2;
+    return c_arr[length/2];
+}
+
+unsigned char find_mean(unsigned char* array, unsigned int length){
+    unsigned long sum = 0;
+    for (unsigned int i = 0; i < length; ++i)
+        sum += array[i];
+    return (unsigned char) sum/length;
+}
+
+
+unsigned char find_maximum(unsigned char* array, unsigned int length){
+    unsigned char max = array[0];
+    for (unsigned int i = 1; i < length; ++i)
+        if (array[i] > max)
+            max = array[i];
+    
+    return max;
+}
+
+unsigned char find_minimum(unsigned char* array, unsigned int length){
+    unsigned char min = array[0];
+    for (unsigned int i = 1; i < length; ++i)
+        if (array[i] < min)
+            min = array[i];
+    
+    return min;
+}
+
+void merge(unsigned char* array,unsigned int l, unsigned int q, unsigned int r){
+    /* creating copies of two sub arrays: */
+    unsigned int n1, n2;
+    n1 = q-l;
+    n2 = r-q;
+    unsigned char left_arr[n1];
+    for (int i = 0; i < n1; ++i)
+        left_arr[i] = array[i+l];
+    unsigned char right_arr[n2];
+    for (int i = 0; i < n2; ++i)
+        right_arr[i] = array[i+q];
+
+    /* merging left_arr and right_arr to array: */
+    unsigned int l_i = 0, r_i = 0, i = l;
+    while(l_i < n1 && r_i < n2)
+    {
+        if (left_arr[l_i] > right_arr[r_i]){
+            array[i] = right_arr[r_i];
+            r_i++;
+        }
+        else{
+            array[i] = left_arr[l_i];
+            l_i++;
+        }
+        i++;
+    }
+
+    while (l_i < n1){
+        array[i] = left_arr[l_i];
+        l_i++;
+        i++;
+    }
+    
+    while (r_i < n2){
+        array[i] = right_arr[r_i];
+        r_i++;
+        i++;
+    }
+}
+
+void mergeSort(unsigned char* array,unsigned int l,unsigned int r){
+    if (r-l < 2)
+        return;
+    unsigned int q = (l+r)/2;
+    mergeSort(array,l,q);
+    mergeSort(array, q, r);
+    merge(array,l,q,r);
+    //print_array(array, size);
+}
+
+void sort_array(unsigned char* array, unsigned int length){
+    mergeSort(array,0,length);
+}
--- stats.h
+++ stats.h
@@ -36,5 +36,49 @@
  * @return <Add Return Informaiton here>
  */
 
+void print_statistics(unsigned char* array, unsigned int length);
+
+void print_array(unsigned char* array, unsigned int length);
+
+unsigned char find_median(unsigned char* array, unsigned int length);
+
+unsigned char find_mean(unsigned char* array, unsigned int length);
+
+unsigned char find_maximum(unsigned char* array, unsigned int length);
+
+unsigned char find_minimum(unsigned char* array, unsigned int length);
+
+void sort_array(unsigned char* array, unsigned int length);
+
+/**=====================    Helper functions     =========================================*/
+/**
+ * @brief merge sort algorithm
+ *
+ * Merge sort algorithm sorting an array by divide-and-conquer approach
+ * The algorithm devied an array by two recursively and then merging
+ * them via megre functoin.
+ * Sorting the array from index 'l' to index 'r'
+ *
+ * @param array  a pointer to an unsorted array of type char
+ * @param l          starting sorting index array of type unsigned int
+ * @param r          final sorting index array of  type unsined int
+ */
+
+void mergeSort(unsigned char* array,unsigned int l,unsigned int r);
+
+/**
+ * @brief helper function to mergeSort, merging between two sorted array
+ *
+ * merge function receive three indexes (l,q,r) and an array
+ * array is sorted between indexes (l,q) and indexes (q,r)
+ * the function returns a sorted array between indexes (l,r)
+ *
+ * @param array : pointer to the initial position of the array
+ * @param l          : index of the first sorted array
+ * @param q          : index of the seconed sorted array
+ * @param r          : index of the final second array position
+ *
+ */
+void merge(unsigned char* array,unsigned int l, unsigned int q, unsigned int r);
 
 #endif /* __STATS_H__ */
